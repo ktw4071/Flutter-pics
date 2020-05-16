b5b163847514be199aa69b18cfa088ef78010ec5
Taewoo Kim <taewkim@cisco.com>
2020-05-16 17:36:16 -0400

    change comments

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,6 @@ import 'package:flutter/material.dart';
 import 'src/app.dart';
 // 2. Define a 'main' function to run when our app starts
 void main() {
-  // 3. Create a new text widget to show some text
-  // on the screen
-
-// 4. Take that widget get it on the screen
+  // using imported library
   runApp(App());
 }
