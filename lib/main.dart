33c3061e9f889498e4e28de033128e74762e41ac
Taewoo Kim <taewkim@cisco.com>
2020-05-16 17:34:57 -0400

    import custom widget library

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,12 @@
 // 1. I need to import a helper library
 // from flutter to get content on the screen
 import 'package:flutter/material.dart';
+import 'src/app.dart';
 // 2. Define a 'main' function to run when our app starts
 void main() {
   // 3. Create a new text widget to show some text
   // on the screen
-  var app = MaterialApp(
-    home: Scaffold(
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          print('Hi there!');
-        },
-      ),
-      appBar: AppBar(
-        title: Text('Lets see some images!!!')
-      )
-    )
-  );
 
 // 4. Take that widget get it on the screen
-  runApp(app);
+  runApp(App());
 }
