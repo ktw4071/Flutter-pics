ce394a62eff9059a2f37793d569d4002d4f799cd
Taewoo Kim <taewkim@cisco.com>
2020-05-16 23:59:50 -0400

    adding http library

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
 
 class App extends StatefulWidget {
   createState() {
@@ -10,7 +11,8 @@ class AppState extends State<App> {
   int counter = 0;
 
   void fetchImage() {
-
+    counter += 1;
+    get('https://jsonplaceholder.typicode.com/photos/$counter');
   }
 
   Widget build(context) {
