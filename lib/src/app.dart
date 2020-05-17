9c12f3b51f43b6986415ed3efcc107c501759955
Taewoo Kim <taewkim@cisco.com>
2020-05-17 00:09:21 -0400

    add list variable and update state using setState

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -11,11 +11,15 @@ class App extends StatefulWidget {
 
 class AppState extends State<App> {
   int counter = 0;
+  List<ImageModel> images = [];
 
   void fetchImage() async {
     counter++;
     var response = await get('https://jsonplaceholder.typicode.com/photos/$counter');
     var imageModel = ImageModel.fromJson(json.decode(response.body));
+    setState(() {
+      images.add(imageModel);
+    });
   }
 
   Widget build(context) {
