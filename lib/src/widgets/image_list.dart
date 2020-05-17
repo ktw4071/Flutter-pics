05a8bba9765f92a0bf76923a6a79a5501676b0da
Taewoo Kim <taewkim@cisco.com>
2020-05-17 00:26:04 -0400

    using final keyword for variable in Stateless Widget

--- lib/src/widgets/image_list.dart
+++ lib/src/widgets/image_list.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import '../models/image_model.dart';
 
 class ImageList extends StatelessWidget {
-  List<ImageModel> images;
+  final List<ImageModel> images;
 
   ImageList(this.images);
 
